ae2fde91bff7c428280e868b3b8251a4deabbcb1
Herbie Bradley <mail@herbiebradley.com>
2018-11-20 22:07:53 +0000

    Validate checkpoints are working

--- src/models/cyclegan.py
+++ src/models/cyclegan.py
@@ -8,20 +8,20 @@ import time
 import multiprocessing
 import glob
 
-""" Define Hyperparameters"""
+"""Define Hyperparameters"""
 project_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir + os.sep + os.pardir))
 learning_rate = 0.0002
 batch_size = 1 # Set batch size to 4 or 16 if training multigpu
 img_size = 256
 cyc_lambda = 10
-epochs = 5
-trainA_path = os.path.join(project_dir, "data", "raw", "horse2zebra", "trainA")
-trainB_path = os.path.join(project_dir, "data", "raw", "horse2zebra", "trainB")
+epochs = 20
+trainA_path = os.path.join(project_dir, 'data', 'raw', 'horse2zebra', 'trainA')
+trainB_path = os.path.join(project_dir, 'data', 'raw', 'horse2zebra', 'trainB')
 trainA_size = len(os.listdir(trainA_path))
 trainB_size = len(os.listdir(trainB_path))
-batches_per_epoch = int((trainA_size + trainB_size) / (2* batch_size)) # Average dataset size / batch_size
+batches_per_epoch = int((trainA_size + trainB_size) / (2 * batch_size)) # Average dataset size / batch_size
 
-""" Load Datasets"""
+"""Load Datasets"""
 
 def load_image(image_file):
     image = tf.read_file(image_file)
@@ -32,32 +32,32 @@ def load_image(image_file):
     return image
 
 def download_data(download_location):
-    path_to_zip = tf.keras.utils.get_file("horse2zebra.zip", cache_subdir=os.path.abspath(download_location),
-        origin="https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/horse2zebra.zip",
+    path_to_zip = tf.keras.utils.get_file('horse2zebra.zip', cache_subdir=os.path.abspath(download_location),
+        origin='https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/horse2zebra.zip',
         extract=True)
     os.remove(path_to_zip)
 
 def load_data(batch_size=batch_size, download=False):
-    raw_data = os.path.join(project_dir, "data", "raw")
+    raw_data = os.path.join(project_dir, 'data', 'raw')
     if download:
         download_data(download_location=raw_data)
 
-    path_to_dataset = os.path.join(raw_data, "horse2zebra/")
-    trainA_path = os.path.join(path_to_dataset, "trainA")
-    trainB_path = os.path.join(path_to_dataset, "trainB")
+    path_to_dataset = os.path.join(raw_data, 'horse2zebra/')
+    trainA_path = os.path.join(path_to_dataset, 'trainA')
+    trainB_path = os.path.join(path_to_dataset, 'trainB')
 
     trainA_size = len(os.listdir(trainA_path))
     trainB_size = len(os.listdir(trainB_path))
     threads = multiprocessing.cpu_count()
 
-    train_datasetA = tf.data.Dataset.list_files(trainA_path + os.sep + "*.jpg", shuffle=False)
+    train_datasetA = tf.data.Dataset.list_files(trainA_path + os.sep + '*.jpg', shuffle=False)
     train_datasetA = train_datasetA.apply(tf.contrib.data.shuffle_and_repeat(buffer_size=trainA_size))
     train_datasetA = train_datasetA.apply(tf.contrib.data.map_and_batch(lambda x: load_image(x),
                                                             batch_size=batch_size,
                                                             num_parallel_calls=threads))
     train_datasetA = train_datasetA.prefetch(buffer_size=batch_size)
 
-    train_datasetB = tf.data.Dataset.list_files(trainB_path + os.sep + "*.jpg", shuffle=False)
+    train_datasetB = tf.data.Dataset.list_files(trainB_path + os.sep + '*.jpg', shuffle=False)
     train_datasetB = train_datasetB.apply(tf.contrib.data.shuffle_and_repeat(buffer_size=trainB_size))
     train_datasetB = train_datasetB.apply(tf.contrib.data.map_and_batch(lambda x: load_image(x),
                                                             batch_size=batch_size,
@@ -66,7 +66,7 @@ def load_data(batch_size=batch_size, download=False):
 
     return train_datasetA, train_datasetB
 
-""" Define CycleGAN architecture"""
+"""Define CycleGAN architecture"""
 
 class Encoder(tf.keras.Model):
 
@@ -75,11 +75,11 @@ class Encoder(tf.keras.Model):
 
         # Small variance in initialization helps with preventing colour inversion.
         self.conv1 = tf.keras.layers.Conv2D(32, kernel_size=7, strides=1, kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
-        self.conv2 = tf.keras.layers.Conv2D(64, kernel_size=3, strides=2, padding="same", kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
-        self.conv3 = tf.keras.layers.Conv2D(128, kernel_size=3, strides=2, padding="same", kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
+        self.conv2 = tf.keras.layers.Conv2D(64, kernel_size=3, strides=2, padding='same', kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
+        self.conv3 = tf.keras.layers.Conv2D(128, kernel_size=3, strides=2, padding='same', kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
 
     def call(self, inputs, training=True):
-        x = tf.pad(inputs, [[0, 0], [3, 3], [3, 3], [0, 0]], "REFLECT")
+        x = tf.pad(inputs, [[0, 0], [3, 3], [3, 3], [0, 0]], 'REFLECT')
 
         x = self.conv1(x)
         x = tf.contrib.layers.instance_norm(x, epsilon=1e-05, trainable=training)
@@ -105,18 +105,18 @@ class Residual(tf.keras.Model):
         self.conv2 = tf.keras.layers.Conv2D(128, kernel_size=3, strides=1, kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
 
     def call(self, inputs, training=True):
-        x = tf.pad(inputs, [[0, 0], [1, 1], [1, 1], [0, 0]], "REFLECT")
+        x = tf.pad(inputs, [[0, 0], [1, 1], [1, 1], [0, 0]], 'REFLECT')
 
         x = self.conv1(x)
         x = tf.contrib.layers.instance_norm(x, epsilon=1e-05, trainable=training)
         x = tf.nn.relu(x)
 
-        x = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], "REFLECT")
+        x = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], 'REFLECT')
 
         x = self.conv2(x)
         x = tf.contrib.layers.instance_norm(x, epsilon=1e-05, trainable=training)
 
-        x = tf.add(x, inputs)
+        x = tf.add(x, inputs) # Consider concatenating in channel dimension here for better net
         return x
 
 
@@ -125,8 +125,8 @@ class Decoder(tf.keras.Model):
     def __init__(self):
         super(Decoder, self).__init__()
 
-        self.conv1 = tf.keras.layers.Conv2DTranspose(64, kernel_size=3, strides=2, padding="same", kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
-        self.conv2 = tf.keras.layers.Conv2DTranspose(32, kernel_size=3, strides=2, padding="same", kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
+        self.conv1 = tf.keras.layers.Conv2DTranspose(64, kernel_size=3, strides=2, padding='same', kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
+        self.conv2 = tf.keras.layers.Conv2DTranspose(32, kernel_size=3, strides=2, padding='same', kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
         self.conv3 = tf.keras.layers.Conv2D(3, kernel_size=7, strides=1, kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
 
     def call(self, inputs, training=True):
@@ -138,7 +138,7 @@ class Decoder(tf.keras.Model):
         x = tf.contrib.layers.instance_norm(x, epsilon=1e-05, trainable=training)
         x = tf.nn.relu(x)
 
-        x = tf.pad(x, [[0, 0], [3, 3], [3, 3], [0, 0]], "REFLECT")
+        x = tf.pad(x, [[0, 0], [3, 3], [3, 3], [0, 0]], 'REFLECT')
 
         x = self.conv3(x)
         x = tf.contrib.layers.instance_norm(x, epsilon=1e-05, trainable=training)
@@ -202,11 +202,11 @@ class Discriminator(tf.keras.Model):
     def __init__(self):
         super(Discriminator, self).__init__()
 
-        self.conv1 = tf.keras.layers.Conv2D(64, kernel_size=4, strides=2, padding="same", kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
-        self.conv2 = tf.keras.layers.Conv2D(128, kernel_size=4, strides=2, padding="same", kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
-        self.conv3 = tf.keras.layers.Conv2D(256, kernel_size=4, strides=2, padding="same", kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
-        self.conv4 = tf.keras.layers.Conv2D(512, kernel_size=4, strides=1, padding="same", kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
-        self.conv5 = tf.keras.layers.Conv2D(1, kernel_size=4, strides=1, padding="same", kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
+        self.conv1 = tf.keras.layers.Conv2D(64, kernel_size=4, strides=2, padding='same', kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
+        self.conv2 = tf.keras.layers.Conv2D(128, kernel_size=4, strides=2, padding='same', kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
+        self.conv3 = tf.keras.layers.Conv2D(256, kernel_size=4, strides=2, padding='same', kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
+        self.conv4 = tf.keras.layers.Conv2D(512, kernel_size=4, strides=1, padding='same', kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
+        self.conv5 = tf.keras.layers.Conv2D(1, kernel_size=4, strides=1, padding='same', kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
 
         self.leaky = tf.keras.layers.LeakyReLU(0.2)
 
@@ -273,62 +273,61 @@ def generate_images(fake_A, fake_B):
         plt.title(title[i])
         # getting the pixel values between [0, 1] to plot it.
         plt.imshow(display_list[i] * 0.5 + 0.5)
-        plt.axis("off")
+        plt.axis('off')
     plt.show()
 
 def define_checkpoint(checkpoint_dir, model):
     nets, optimizers = model
-    discA = nets["discA"]
-    discB = nets["discB"]
-    genA2B = nets["genA2B"]
-    genB2A = nets["genB2A"]
-    discA_opt = optimizers["discA_opt"]
-    discB_opt = optimizers["discB_opt"]
-    genA2B_opt = optimizers["genA2B_opt"]
-    genB2A_opt = optimizers["genB2A_opt"]
+    discA = nets['discA']
+    discB = nets['discB']
+    genA2B = nets['genA2B']
+    genB2A = nets['genB2A']
+    discA_opt = optimizers['discA_opt']
+    discB_opt = optimizers['discB_opt']
+    genA2B_opt = optimizers['genA2B_opt']
+    genB2A_opt = optimizers['genB2A_opt']
 
-    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+    step_counter = tf.train.get_or_create_global_step()
     checkpoint = tf.train.Checkpoint(discA=discA, discB=discB, genA2B=genA2B, genB2A=genB2A,
                                  discA_opt=discA_opt, discB_opt=discB_opt,
                                  genA2B_opt=genA2B_opt, genB2A_opt=genB2A_opt,
-                                 optimizer_step=tf.train.get_or_create_global_step())
-    return checkpoint, checkpoint_dir, checkpoint_prefix
+                                 optimizer_step=step_counter)
+    return checkpoint, checkpoint_dir
 
 def restore_from_checkpoint(checkpoint, checkpoint_dir):
     latest_checkpoint = tf.train.latest_checkpoint(checkpoint_dir)
     if latest_checkpoint is not None:
-        try:
-            checkpoint.restore(latest_checkpoint).assert_consumed()
-            print("Checkpoint restored")
-        except:
-            print("Restore failed")
+        # Use assert_existing_objects_matched() instead of asset_consumed() here because
+        # optimizers aren't initialized fully until first gradient update
+        restore_obj = checkpoint.restore(latest_checkpoint).assert_existing_objects_matched()
+        print("Checkpoint restored from ", latest_checkpoint)
+        # Uncomment below to print full list of checkpoint metadata
+        #print(tf.contrib.checkpoint.object_metadata(latest_checkpoint))
     else:
         print("No checkpoint found, initializing model.")
 
-def define_model(learning_rate, training=True): # Init only generators for testing
-    discA = Discriminator()
-    discB = Discriminator()
-    genA2B = Generator()
-    genB2A = Generator()
-    discA_opt = tf.train.AdamOptimizer(learning_rate, beta1=0.5)
-    discB_opt = tf.train.AdamOptimizer(learning_rate, beta1=0.5)
-    genA2B_opt = tf.train.AdamOptimizer(learning_rate, beta1=0.5)
-    genB2A_opt = tf.train.AdamOptimizer(learning_rate, beta1=0.5)
+def define_model(learning_rate, training=True):
+    if not training:
+        genA2B = Generator()
+        genB2A = Generator()
+        return {'genA2B':genA2B, 'genB2A':genB2A}
+    else:
+        discA = Discriminator()
+        discB = Discriminator()
+        genA2B = Generator()
+        genB2A = Generator()
+        discA_opt = tf.train.AdamOptimizer(learning_rate)
+        discB_opt = tf.train.AdamOptimizer(learning_rate, beta1=0.5)
+        genA2B_opt = tf.train.AdamOptimizer(learning_rate, beta1=0.5)
+        genB2A_opt = tf.train.AdamOptimizer(learning_rate, beta1=0.5)
 
-    nets = {"discA":discA, "discB":discB, "genA2B":genA2B, "genB2A":genB2A}
-    optimizers = {"discA_opt":discA_opt, "discB_opt":discB_opt, "genA2B_opt":genA2B_opt, "genB2A_opt":genB2A_opt}
-    return nets, optimizers
+        nets = {'discA':discA, 'discB':discB, 'genA2B':genA2B, 'genB2A':genB2A}
+        optimizers = {'discA_opt':discA_opt, 'discB_opt':discB_opt, 'genA2B_opt':genA2B_opt, 'genB2A_opt':genB2A_opt}
+        return nets, optimizers
 
 def test(data, model, checkpoints):
-    nets, optimizers = model
-    discA = nets["discA"]
-    discB = nets["discB"]
-    genA2B = nets["genA2B"]
-    genB2A = nets["genB2A"]
-    discA_opt = optimizers["discA_opt"]
-    discB_opt = optimizers["discB_opt"]
-    genA2B_opt = optimizers["genA2B_opt"]
-    genB2A_opt = optimizers["genB2A_opt"]
+    genA2B = model['genA2B']
+    genB2A = model['genB2A']
 
     checkpoint, checkpoint_dir, checkpoint_prefix = checkpoints
     restore_from_checkpoint(checkpoint, checkpoint_dir)
@@ -346,36 +345,39 @@ def test(data, model, checkpoints):
 
         genA2B_output = genA2B(testA, training=False)
         genB2A_output = genB2A(testB, training=False)
-        generate_images(genB2A_output, genA2genA2B_output)
+        generate_images(genB2A_output, genA2B_output)
 
 
-def train(data, model, checkpoints, epochs, learning_rate=learning_rate, lsgan=True):
+def train(data, model, checkpoint_data, epochs, learning_rate=learning_rate, lsgan=True):
     nets, optimizers = model
-    discA = nets["discA"]
-    discB = nets["discB"]
-    genA2B = nets["genA2B"]
-    genB2A = nets["genB2A"]
-    discA_opt = optimizers["discA_opt"]
-    discB_opt = optimizers["discB_opt"]
-    genA2B_opt = optimizers["genA2B_opt"]
-    genB2A_opt = optimizers["genB2A_opt"]
+    discA = nets['discA']
+    discB = nets['discB']
+    genA2B = nets['genA2B']
+    genB2A = nets['genB2A']
+    discA_opt = optimizers['discA_opt']
+    discB_opt = optimizers['discB_opt']
+    genA2B_opt = optimizers['genA2B_opt']
+    genB2A_opt = optimizers['genB2A_opt']
 
-    checkpoint, checkpoint_dir, checkpoint_prefix = checkpoints
+    checkpoint, checkpoint_dir = checkpoint_data
+    checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt')
     restore_from_checkpoint(checkpoint, checkpoint_dir)
+
     train_datasetA, train_datasetB = iter(data[0]), iter(data[1])
+    global_step = tf.train.get_or_create_global_step()
+
 
     for epoch in range(epochs):
         start = time.time()
         for train_step in range(batches_per_epoch):
-            with tf.GradientTape() as genA2B_tape, tf.GradientTape() as genB2A_tape, \
-                tf.GradientTape() as discA_tape, tf.GradientTape() as discB_tape:
-                try:
-                    # Get next training minibatches
-                    trainA = next(train_datasetA)
-                    trainB = next(train_datasetB)
-                except tf.errors.OutOfRangeError:
-                    print("Error, run out of data")
-                    break
+            try:
+                # Get next training minibatches
+                trainA = next(train_datasetA)
+                trainB = next(train_datasetB)
+            except tf.errors.OutOfRangeError:
+                print("Error, run out of data")
+                break
+            with tf.GradientTape(persistent=True) as tape:
 
                 genA2B_output = genA2B(trainA, training=True)
                 genB2A_output = genB2A(trainB, training=True)
@@ -396,34 +398,29 @@ def train(data, model, checkpoints, epochs, learning_rate=learning_rate, lsgan=T
                 genB2A_loss = generator_loss(discA_fake_output, lsgan=lsgan) + \
                               cycle_consistency_loss(trainA, trainB, reconstructedA, reconstructedB)
 
-            genA2B_gradients = genA2B_tape.gradient(genA2B_loss, genA2B.variables)
-            genB2A_gradients = genB2A_tape.gradient(genB2A_loss, genB2A.variables)
-            discA_gradients = discA_tape.gradient(discA_loss, discA.variables)
-            discB_gradients = discB_tape.gradient(discB_loss, discB.variables)
+            discA_gradients = tape.gradient(discA_loss, discA.variables)
+            discB_gradients = tape.gradient(discB_loss, discB.variables)
+            genA2B_gradients = tape.gradient(genA2B_loss, genA2B.variables)
+            genB2A_gradients = tape.gradient(genB2A_loss, genB2A.variables)
 
-            genA2B_opt.apply_gradients(zip(genA2B_gradients, genA2B.variables),
-                                       global_step=tf.train.get_or_create_global_step())
-            genB2A_opt.apply_gradients(zip(genB2A_gradients, genB2A.variables),
-                                       global_step=tf.train.get_or_create_global_step())
-            discA_opt.apply_gradients(zip(discA_gradients, discA.variables),
-                                      global_step=tf.train.get_or_create_global_step())
-            discB_opt.apply_gradients(zip(discB_gradients, discB.variables),
-                                      global_step=tf.train.get_or_create_global_step())
+            discA_opt.apply_gradients(zip(discA_gradients, discA.variables), global_step=global_step)
+            discB_opt.apply_gradients(zip(discB_gradients, discB.variables), global_step=global_step)
+            genA2B_opt.apply_gradients(zip(genA2B_gradients, genA2B.variables), global_step=global_step)
+            genB2A_opt.apply_gradients(zip(genB2A_gradients, genB2A.variables), global_step=global_step)
 
             if train_step % 100 == 0:
                 print("Training step: ", train_step)
-            # saving (checkpoint) the model
-            #if (epoch + 1) % 3 == 0:
-            checkpoint.save(file_prefix=checkpoint_prefix)
-            print("Checkpoint saved at ", checkpoint_prefix)
-
+        # checkpoint the model
+        if (epoch + 1) % 3 == 0:
+            checkpoint_path = checkpoint.save(file_prefix=checkpoint_prefix)
+            print("Checkpoint saved at ", checkpoint_path)
         print ("Time taken for epoch {} is {} sec\n".format(epoch + 1, time.time()-start))
 
 if __name__ == "__main__":
-    checkpoint_dir = os.path.join(project_dir, "models", "checkpoints")
+    checkpoint_dir = os.path.join(project_dir, 'models', 'checkpoints')
     with tf.device("/cpu:0"):
         data = load_data(batch_size=batch_size)
         model = define_model(learning_rate=learning_rate)
-        checkpoints = define_checkpoint(checkpoint_dir, model)
-    with tf.device("/gpu:0"):
-        train(data, model, checkpoints, epochs=epochs, learning_rate=learning_rate, lsgan=True)
+        checkpoint_data = define_checkpoint(checkpoint_dir, model)
+    #with tf.device("/gpu:0"):
+        train(data, model, checkpoint_data, epochs=epochs, learning_rate=learning_rate, lsgan=True)
