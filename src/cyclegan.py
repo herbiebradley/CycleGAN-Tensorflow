Fix normalization for loading and saving images

src/cyclegan.py
@@ -11,7 +11,6 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-import utils
 import models
 from preprocessing.load_data import load_train_data, load_test_data, save_images
 from models.losses import generator_loss, discriminator_loss, cycle_consistency_loss
@@ -28,7 +27,7 @@ batch_size = 1 # Set batch size to 4 or 16 if training multigpu
 img_size = 256
 cyc_lambda = 10
 epochs = 2
-batches_per_epoch = data.get_batches_per_epoch(dataset_id, project_dir)
+batches_per_epoch = models.get_batches_per_epoch(dataset_id, project_dir)
 
 def define_checkpoint(checkpoint_dir, model, training=True):
     if not training:
@@ -104,7 +103,6 @@ def test(data, model, checkpoint_info, dataset_id):
     test_datasetA, test_datasetB, testA_size, testB_size = data
     test_datasetA = iter(test_datasetA)
     test_datasetB = iter(test_datasetB)
-    testA = next(test_datasetA)
 
     for imageB in range(testB_size):
         start = time.time()
@@ -204,37 +202,33 @@ def train(data, model, checkpoint_info, epochs, initial_learning_rate=initial_le
                     genB2A_opt.apply_gradients(zip(genB2A_gradients, genB2A.variables), global_step=global_step)
 
                     # Summaries
-                    #tf.contrib.summary.scalar('train_step', global_step // 4)
                     tf.contrib.summary.scalar('loss/genA2B', genA2B_loss)
                     tf.contrib.summary.scalar('loss/genB2A', genB2A_loss)
                     tf.contrib.summary.scalar('loss/discA', discA_loss)
                     tf.contrib.summary.scalar('loss/discB', discB_loss)
                     tf.contrib.summary.scalar('loss/cyc', cyc_loss)
-                    #tf.contrib.summary.scalar('learning_rate', learning_rate)
+                    tf.contrib.summary.scalar('learning_rate', learning_rate)
 
-                    #tf.contrib.summary.histogram('discA/real', discA_real)
-                    #tf.contrib.summary.histogram('discA/fake', discA_fake)
-                    #tf.contrib.summary.histogram('discB/real', discB_real)
-                    #tf.contrib.summary.histogram('discB/fake', discA_fake)
-
-                    #tf.contrib.summary.image('A/generated', genB2A_output)
-                    #tf.contrib.summary.image('trainA', trainA)
-                    #tf.contrib.summary.image('trainB', trainB)
-                    #tf.contrib.summary.image('A/reconstructed', reconstructedA)
-                    #tf.contrib.summary.image('B/generated', genA2B_output)
-                    #tf.contrib.summary.image('B/reconstructed', reconstructedB)
+                    tf.contrib.summary.histogram('discA/real', discA_real)
+                    tf.contrib.summary.histogram('discA/fake', discA_fake)
+                    tf.contrib.summary.histogram('discB/real', discB_real)
+                    tf.contrib.summary.histogram('discB/fake', discA_fake)
+                    # Transform images from [-1, 1] to [0, 1) for Tensorboard.
+                    tf.contrib.summary.image('A/generated', (genB2A_output * 0.5) + 0.5)
+                    tf.contrib.summary.image('A/reconstructed', (reconstructedA * 0.5) + 0.5)
+                    tf.contrib.summary.image('B/generated', (genA2B_output * 0.5) + 0.5)
+                    tf.contrib.summary.image('B/reconstructed', (reconstructedB * 0.5) + 0.5)
 
                     if train_step % 100 == 0:
                         # Here we do global step / 4 because there are 4 gradient updates per batch.
                         print("Global Training Step: ", global_step.numpy() // 4)
                         print("Epoch Training Step: ", train_step + 1)
         # Assign decayed learning rate:
-        learning_rate.assign(utils.get_learning_rate(initial_learning_rate, global_step,
-                                                     batches_per_epoch))
+        learning_rate.assign(models.get_learning_rate(initial_learning_rate, global_step, batches_per_epoch))
         print("Learning rate in total epoch {} is: {}".format(global_step.numpy() // (4 * batches_per_epoch),
-                                                        learning_rate.numpy()))
+                                                            learning_rate.numpy()))
         # Checkpoint the model:
-        if (epoch + 1) % 1 == 0:
+        if (epoch + 1) % 2 == 0:
             checkpoint_path = checkpoint.save(file_prefix=checkpoint_prefix)
             print("Checkpoint saved at ", checkpoint_path)
         print ("Time taken for local epoch {} is {} sec\n".format(epoch + 1, time.time()-start))
@@ -244,7 +238,7 @@ if __name__ == "__main__":
     with tf.device("/cpu:0"): # Preprocess data on CPU for significant performance gains.
         data = load_test_data(dataset_id, project_dir)
 
-    #with tf.device("/gpu:0"):
-        #model = define_model(initial_learning_rate=initial_learning_rate, training=False)
-        #checkpoint_info = define_checkpoint(checkpoint_dir, model, training=False)
-        #test(data, model, checkpoint_info, dataset_id)
+    with tf.device("/gpu:0"):
+        model = define_model(initial_learning_rate=initial_learning_rate, training=False)
+        checkpoint_info = define_checkpoint(checkpoint_dir, model, training=False)
+        test(data, model, checkpoint_info, dataset_id)

src/preprocessing/load_data.py
@@ -6,29 +6,34 @@ import os
 import multiprocessing
 
 import tensorflow as tf
-from PIL import Image
 
 def load_image(image_file, img_size=256):
     # Read file into tensor of type string.
-    image = tf.read_file(image_file)
+    image_string = tf.read_file(image_file)
     # Decodes file into jpg of type uint8 (range [0, 255]).
-    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.decode_jpeg(image_string, channels=3)
     # Convert to floating point with 32 bits (range [0, 1]).
     image = tf.image.convert_image_dtype(image, tf.float32)
-    # TODO: Replace with PIL
-    image = tf.image.resize_images(image, size=[img_size, img_size])
-    image = tf.image.per_image_standardization(image)
-    image = (image * 0.5) # Transform image to [-1, 1]
-    return image
+    # Resize with bicubic interpolation, making sure that corner pixel values
+    # are preserved.
+    image = tf.image.resize_images(image, size=[img_size, img_size],
+                                   method=tf.image.ResizeMethod.BICUBIC, align_corners=True)
+    # Transform image to [-1, 1] from [0, 1].
+    return (image - 0.5) * 2
 
-def save_images(image_to_save, save_dir, image_index):
+def save_images(image_to_save, save_dir, image_index, img_size=256):
     save_file = os.path.join(save_dir,'test' + str(image_index) + '.jpg')
+    # Reshape to get rid of batch size dimension in the tensor.
     image = tf.reshape(image_to_save, shape=[img_size, img_size, 3])
-    image = (image + 1) * 127.5 # Rescale images to [0, 255]
+    # Scale from [-1, 1] to [0, 1).
+    image = (image * 0.5) + 0.5
+    # Convert to uint8 (range [0, 255]), saturate to avoid possible under/overflow.
     image = tf.image.convert_image_dtype(image, dtype=tf.uint8, saturate=True)
+    # JPEG encode image into string Tensor.
     image_string = tf.image.encode_jpeg(image, format='rgb', quality=95)
-    tf.write_file(save_file, image_string)
+    tf.write_file(filename=save_file, contents=image_string)
 
+# TODO: Refactor into class
 def load_train_data(dataset_id, project_dir, batch_size=1):
     path_to_dataset = os.path.join(project_dir, 'data', 'raw', dataset_id + os.sep)
     trainA_path = os.path.join(path_to_dataset, 'trainA')
@@ -83,22 +88,15 @@ def load_test_data(dataset_id, project_dir):
                                                             batch_size=1,
                                                             num_parallel_calls=threads,
                                                             drop_remainder=False))
-    #test_datasetA = test_datasetA.prefetch(buffer_size=threads)
-    #test_datasetA = test_datasetA.apply(tf.contrib.data.prefetch_to_device("/gpu:0", buffer_size=1))
+    test_datasetA = test_datasetA.prefetch(buffer_size=threads)
+    test_datasetA = test_datasetA.apply(tf.contrib.data.prefetch_to_device("/gpu:0", buffer_size=1))
 
     test_datasetB = tf.data.Dataset.list_files(testB_path + os.sep + '*.jpg', shuffle=False)
     test_datasetB = test_datasetB.apply(tf.contrib.data.map_and_batch(lambda x: load_image(x),
                                                             batch_size=1,
                                                             num_parallel_calls=threads,
                                                             drop_remainder=False))
-    #test_datasetB = test_datasetB.prefetch(buffer_size=threads)
-    #test_datasetB = test_datasetB.apply(tf.contrib.data.prefetch_to_device("/gpu:0", buffer_size=1))
-
-    test_datasetA = iter(test_datasetB)
-    testA = next(test_datasetA)
-    print("A Max: ", tf.reduce_max(testA))
-    print("A Min: ", tf.reduce_min(testA))
-    print("A Mean: ", tf.reduce_mean(testA))
-    print(testA)
+    test_datasetB = test_datasetB.prefetch(buffer_size=threads)
+    test_datasetB = test_datasetB.apply(tf.contrib.data.prefetch_to_device("/gpu:0", buffer_size=1))
 
     return test_datasetA, test_datasetB, testA_size, testB_size