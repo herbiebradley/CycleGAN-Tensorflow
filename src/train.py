d6faa41ef58932e6b7be7956dba689fd91c1ff58
herbiebradley <hyperionbradley@gmail.com>
2019-02-26 14:55:54 +0000

    Small fix to train.py

--- src/train.py
+++ src/train.py
@@ -16,7 +16,7 @@ tf.enable_eager_execution()
 """Hyperparameters (TODO: Move to argparse)"""
 project_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 dataset_id = 'facades'
-epochs = 15
+epochs = 50
 batches_per_epoch = models.get_batches_per_epoch(dataset_id, project_dir)
 batch_size = 1 # Set batch size to 4 or 16 if training multigpu
 initial_learning_rate = 0.0002
@@ -47,6 +47,7 @@ if __name__ == "__main__":
                                  identity_lambda, checkpoint_dir, img_size,
                                  training=True)
     with tf.device("/gpu:0"):
+        global_step = model.global_step
         # Initialize Tensorboard summary writer:
         log_dir = os.path.join(project_dir, 'saved_models', 'tensorboard')
         summary_writer = tf.contrib.summary.create_file_writer(log_dir, flush_millis=10000)
@@ -55,12 +56,12 @@ if __name__ == "__main__":
             with summary_writer.as_default():
                 for train_step in range(batches_per_epoch):
                     # Record summaries every 100 train_steps; there are 3 gradient updates per step.
-                    with tf.contrib.summary.record_summaries_every_n_global_steps(300, global_step=model.global_step):
+                    with tf.contrib.summary.record_summaries_every_n_global_steps(300, global_step=global_step):
                         # Get next training batches:
                         batch = {"A": dataA.get_next(), "B": dataB.get_next()}
                         model.set_input(batch)
                         model.optimize_parameters()
-                        print("Iteration ", train_step)
+
                         # Summaries for Tensorboard:
                         tf.contrib.summary.image('A/generated', model.fakeA)
                         tf.contrib.summary.image('A/reconstructed', model.reconstructedA)
